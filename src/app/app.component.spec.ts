796e7cdcc06c3662430203db5a70f0772b183bd6
tbelcheva <cannabelcheva90@gmail.com>
2019-02-11 17:34:35 +0200

    Update test in app.component.spec to pass

--- src/app/app.component.spec.ts
+++ src/app/app.component.spec.ts
@@ -26,6 +26,6 @@ describe('AppComponent', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.debugElement.nativeElement;
-    expect(compiled.querySelector('h1').textContent).toEqual('Welcome to TestPhantomJS!');
+    expect(compiled.querySelector('h1').innerText).toEqual('Welcome to TestPhantomJS!');
   });
 });
